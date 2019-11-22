7bd42b8cc1710984db9eb86d26e1e018c6e19774
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2019-11-22 19:55:07 -0300

    Corrigindo problema ao servir arquivo e adicionando script de testes

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'src')));
+
 // Servindo os arquivos
 app.get('/', (req, res) => {
-  res.send(path.join(__dirname + 'src/index.html'));
+  res.send(path.join(__dirname + 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`App running on localhost:${port}`);
+  console.log(`App running on http://localhost:${port}`);
 });
